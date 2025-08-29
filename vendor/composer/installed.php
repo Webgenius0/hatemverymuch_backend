06fae3be1fa770e3072855f6093939e42f2e0c0e
webdevmasum <masumislam0018@gmail.com>
2025-08-29 09:20:12 +0600

    jason

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'f526f43e9baab761567a9b7fc82b5d0f15a0efdf',
+        'reference' => '613609a41575b2bf0645d9d4e31d1d8900ddb36d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -388,7 +388,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'f526f43e9baab761567a9b7fc82b5d0f15a0efdf',
+            'reference' => '613609a41575b2bf0645d9d4e31d1d8900ddb36d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -817,8 +817,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '3.0.0',
-                1 => '1.0|2.0|3.0',
+                0 => '1.0|2.0|3.0',
+                1 => '3.0.0',
             ),
         ),
         'psr/simple-cache' => array(
